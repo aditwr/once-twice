3bb24c4ae542adb53e497e15c1bb1c7eb27a3829
adityawahyuramadhan <awahyu148@gmail.com>
2022-07-10 21:10:51 +0700

    menambah animasi saat halaman diload

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,40 @@
+// Animasi Page Load
+const bgPL = document.querySelector(".bg-page-load");
+const frontPL = document.querySelector(".front-page-load");
+const lightPL = document.querySelector(".light-page-load");
+setTimeout(() => {
+  bgPL.classList.add("hidden");
+  frontPL.classList.add("hidden");
+  lightPL.classList.remove("hidden");
+  lightPL.classList.add("animate-downOpacity");
+  setTimeout(() => {
+    lightPL.classList.add("hidden");
+  }, 500);
+}, 3200);
+
+// Hamburger Menu
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const mobileNav = document.querySelector(".mobile-nav");
+hamburgerMenu.addEventListener("click", function () {
+  mobileNav.classList.toggle("hidden");
+});
 
 // Ketika melakukan scrolling, ubah style navbar
-const navbar = document.querySelector('.navbar');
-const navLink = document.querySelectorAll('.nav-link');
+const navbar = document.querySelector(".navbar");
+const navLink = document.querySelectorAll(".nav-link");
+const scrollSign = document.querySelector(".scroll");
 window.onscroll = function () {
-    if (window.pageYOffset > navbar.offsetTop) {
-        navbar.classList.add('navbar-scroll');
-        for (let index = 0; index < navLink.length; index++) {
-            navLink[index].classList.add("text-slate-600", "hover:text-pink-500");
-        }
+  if (window.pageYOffset > navbar.offsetTop) {
+    navbar.classList.add("navbar-scroll");
+    for (let index = 0; index < navLink.length; index++) {
+      navLink[index].classList.add("text-slate-600", "hover:text-pink-500");
     }
-    else {
-        navbar.classList.remove('navbar-scroll');
-        for (let index = 0; index < navLink.length; index++) {
-            navLink[index].classList.remove("text-slate-600", "hover:text-pink-500");
-        }
+    scrollSign.classList.add("hidden");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+    for (let index = 0; index < navLink.length; index++) {
+      navLink[index].classList.remove("text-slate-600", "hover:text-pink-500");
     }
+    scrollSign.classList.remove("hidden");
+  }
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,34 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],
-        arapey: ['Arapey', 'serif'],
-        poiredOne: ['Poiret One', 'cursive'],
-        viaodaLibre: ['Viaoda Libre', 'cursive'],
-      }
+        roboto: ["Roboto", "sans-serif"],
+        arapey: ["Arapey", "serif"],
+        poiredOne: ["Poiret One", "cursive"],
+        viaodaLibre: ["Viaoda Libre", "cursive"],
+      },
+      keyframes: {
+        scroll: {
+          "0%": { height: "0" },
+          "50%": { height: "100%", transform: "translateY(0)" },
+          "100%": { transform: "translateY(100%)" },
+        },
+        blur: {
+          "0%": { "backdrop-filter": "blur(16px)" },
+          "30%": { "backdrop-filter": "blur(0)" },
+          "60%": { "backdrop-filter": "blur(0)" },
+          "100%": { "backdrop-filter": "blur(24px)" },
+        },
+        downOpacity: {
+          "0%": { opacity: "1" },
+          "100%": { opacity: "0" },
+        },
+      },
+      animation: {
+        scroll: "scroll 1.2s linear 0s infinite",
+        blur: "blur 3.6s ease-in",
+        downOpacity: "downOpacity .5s ease-in",
+      },
     },
   },
   plugins: [],
-}
+};
